bc2b29cd4a2cf04ba62d84bbea4ec44b5cf1e82c
Joris Lamberty <joris.lamberty@t-online.de>
2020-10-14 15:47:46 +0200

    Working Thingy

--- DirtyBomb/DirtyBomb.py
+++ DirtyBomb/DirtyBomb.py
@@ -1,15 +1,17 @@
 #! python3
 # DirtyBomb.py - Searches random terms on google to hide your identity between fake requests
 
-import bs4, requests, os, random, time, re
+import bs4, requests, os, random, time, re, logging
 import urllib.parse
 from urllib.parse import urlparse
-USER_AGENT = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:65.0) Gecko/20100101 Firefox/65.0"
+USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36 Edg/83.0.478.61"
 #Gets the random line for the question
-
+logging.basicConfig(filename = 'E:\\Dokumente\\TestPython\\dirtyBombLog.txt', level=logging.INFO, format=' %(asctime)s - %(levelname) s - %(message)s')
+logging.debug('Start of programm')
 
 def getQuestions(afile):
     line = random.choice(open(afile).readlines())
+    logging.debug(f'Extracted Line:"{line}"')
     return line
 #Formats the questions neatly for a google search
 def formatForSearch():
@@ -18,6 +20,7 @@ def formatForSearch():
     words = line.split()
     #Rejoins with plus(Google Searches work with pluses not with spaces)
     searchTerm = '+'.join(words)
+    logging.debug(f'Searchterm:"{searchTerm}"')
     return searchTerm
 
 #Searches on google and clicks on links
@@ -26,6 +29,7 @@ def googleSearch():
     #Ties new and old together
     query = formatForSearch()
     url = f"https://google.com/search?q={query}"
+    logging.info(f'Search for:"{url}"')
     #Open Website Shabang
     headers = {"user-agent": USER_AGENT}
     res = requests.get(url, headers=headers)
@@ -39,21 +43,25 @@ def googleSearch():
             link = anchors[0]['href']
             websites.append(link)
     return websites
-    
+
+
 def openWebsites(sites):
     num = random.randint(0, 4)
     if(num > 0):
         url = random.choice(sites)
         res = requests.get(url)
         res.raise_for_status
+        logging.info(f'Clicked on:"{url}"')
     if(num > 1):
         url1 = random.choice(sites)
         res = requests.get(url1)
         res.raise_for_status      
+        logging.info(f'Clicked on:"{url1}"')
     if(num > 2):
         url2 = random.choice(sites)
         res = requests.get(url2)
         res.raise_for_status    
+        logging.info(f'Clicked on:"{url2}"')
 
 def connected():
     try:
@@ -67,9 +75,9 @@ def execution():
         if(connected):
             try:
                 openWebsites(googleSearch())
-                time.sleep(rand.randint(1800, 3600))
+                logging.info('Succesfull execution')
+                time.sleep(random.randint(1800, 3600))
             except:
-                time.sleep(rand.randint(1800, 3600))
-                
-
+                logging.error('Unseccesfull execution')
+                time.sleep(random.randint(1800, 3600))
 execution()
